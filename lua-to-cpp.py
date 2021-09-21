39e3a245727ffd725c595081eb594a6cf862490e
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-09-21 17:09:58 +1000

    Got beginnings of header working, stared cpp template
    Basic mapping of lua property types to C++ types also
    happens now.

--- lua-to-cpp.py
+++ lua-to-cpp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pprint
 import sys
 import yaml
 
@@ -7,13 +8,29 @@ import click
 from click import secho
 
 
+def map_lua_type_to_cpp_type(property_list):
+    for property in property_list:
+        prop_type = property["type"]
+        if prop_type.lower() == "number":
+            property["cpp_type"] = "float"
+        elif prop_type.lower() == "string":
+            property["cpp_type"] = "std::string"
+        elif prop_type.lower() == "boolean":
+            property["cpp_type"] = "bool"
+        else:
+            secho(f"Unknown property type in YAML: {prop_type}",
+                  fg="red", bold=True)
+            sys.exit(1)
+    return property_list
+
+
 def load_templates_or_die():
     try:
         _cpp_template = Template(
             open('templates/cpp_class.cpp.j2', 'r').read().strip(),
         )
         _hpp_template = Template(
-            open('templates/cpp_class.cpp.j2', 'r').read().strip(),
+            open('templates/cpp_class.hpp.j2', 'r').read().strip(),
         )
         _eg_lua_template = Template(
             open('templates/example.lua.j2', 'r').read().strip(),
@@ -24,6 +41,14 @@ def load_templates_or_die():
         secho(f"{e}\n")
         secho("!!! Aborted !!!")
         sys.exit(1)
+    return _cpp_template, _hpp_template, _eg_lua_template
+
+
+def load_class_spec_from_yaml(yaml_file_obj):
+    # TODO: Exception handling
+    info = yaml.load(yaml_file_obj, Loader=yaml.Loader)
+    return info
+
 
 @click.command()
 @click.argument("in_file", type=click.File("r"), metavar="IN_YAML")
@@ -41,6 +66,25 @@ def main(in_file):
     print(f"Reading class specification from file: {in_file.name}")
 
     _cpp_template, _hpp_template, _eg_lua_template = load_templates_or_die()
+
+    class_spec = load_class_spec_from_yaml(in_file)
+    class_spec = class_spec["class"]
+
+    class_spec["properties"] = map_lua_type_to_cpp_type(class_spec["properties"])
+
+    pprint.pprint(class_spec)
+    print("\n--------------------------\n")
+    pprint.pprint(class_spec["properties"])
+
+    hpp_output = _hpp_template.render(
+        class_spec=class_spec,
+        property_list=class_spec["properties"]
+    )
+
+    with open(f"{class_spec['cpp_name']}.hpp", "w") as hpp_file:
+        hpp_file.write(hpp_output)
+        secho(f"Wrote {len(hpp_output)} bytes to {hpp_file.name}",
+              fg="green", bold=True)
 
 
 if __name__ == "__main__":
